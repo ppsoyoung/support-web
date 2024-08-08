ad2974bc2a22914cda9db212181b430d830f654c
soyoung-labge <soyoung@labgenomics.com>
2024-08-08 14:05:06 +0900

    first commit

---
+++ src/main/java/com/labgenomics/supportweb/SupportWebApplication.java
@@ -0,0 +1,13 @@
+package com.labgenomics.supportweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/labgenomics/supportweb/SupportWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labgenomics.supportweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
